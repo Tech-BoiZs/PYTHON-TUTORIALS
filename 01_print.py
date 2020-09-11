8913df3d33b41c10048a498a1b6066d311c80c97
bphariharan1301 <56637724+bphariharan1301@users.noreply.github.com>
2020-09-11 19:42:50 +0530

    Source Code for print() videos on youtube

---
+++ 01_print.py
@@ -0,0 +1,26 @@
+# Hello World Program
+# print("Hello World")
+
+# print("Hello"," World")
+
+# int, float, char, boolean datatypes
+
+'''a=10
+print(type(a))'''  # datatype of the variable
+
+# O/P: <class 'int'>
+
+'''a=9.4
+print(type(a))''' # O/P: <class 'float'>
+
+'''a=1353.76453
+print(type(a))'''
+
+# boolean 1-True 0-False
+# True and False
+
+a=True
+print(type(a)) # O/P: <class 'bool'>
+
+
+
