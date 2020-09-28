fb2fb503697e83f6417f5db41e7677201fb045d3
bphariharan1301 <56637724+bphariharan1301@users.noreply.github.com>
2020-09-28 22:30:12 +0530

    Input function file added

---
+++ 02_input.py
@@ -0,0 +1,35 @@
+
+
+# var_name = input()
+
+'''a = input()
+
+print(a)'''
+
+# var_name = input("Enter the value")
+
+
+'''a = input("Enter the value")
+
+print(a)'''
+
+a = int(input("Enter the value")) # a-> int type conversion
+b = int(input("Enter the value")) # b-> int type conversion
+# c = input("enter the value") # c-> string typecasting
+# type conversion
+
+# var_name = datatype(var_name)
+
+
+a=a+b
+
+print(a)
+
+'''a = input("Enter the value")
+
+b = input("Enter the value")
+
+a=a*b
+
+print(a)
+'''
